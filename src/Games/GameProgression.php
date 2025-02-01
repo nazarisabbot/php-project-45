fa5791866530677076a8b4af6c9f40981e36f63b
nazar <n.updesign@gmail.com>
2025-02-01 11:05:55 +0500

    ninth step r4

--- src/Games/GameProgression.php
+++ src/Games/GameProgression.php
@@ -15,12 +15,13 @@ function branProgression()
 {
     $generateRound = function (): array {
         $start = rand(0, 12);
-        $end = rand($start, 22);
+        $length = rand(5, 10); // Гарантируем минимум 5 элементов
+        $step = rand(2, 4);
 
-        $arr = getProgression($start, $end);
-        $index = rand(1, count($arr) - 1);
+        $arr = getProgression($start, $length, $step);
+        $index = rand(0, count($arr) - 1); // Выбираем случайный элемент
 
-        $correctAnswer = (string)($arr[$index]);
+        $correctAnswer = (string)$arr[$index];
         $arr[$index] = '..';
 
         $question = implode(" ", $arr);
